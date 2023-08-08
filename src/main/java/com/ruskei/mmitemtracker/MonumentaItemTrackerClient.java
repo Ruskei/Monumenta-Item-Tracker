88bc8211783a6fd421310a5d76881821c2ba480e
russian <dominusfinem@gmail.com>
2023-08-08 13:35:56 -0700

    init

---
+++ src/main/java/com/ruskei/mmitemtracker/MonumentaItemTrackerClient.java
@@ -0,0 +1,10 @@
+package com.ruskei.mmitemtracker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MonumentaItemTrackerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
